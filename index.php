43844e9d804f91c223ac559b7e0ef8c8411690d4
wakana1108 <sugitani@yukisaki.co.jp>
2025-10-02 17:32:59 +0900

    プランセクション作成

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
                 <div class="p-front__about__item p-front__about__item--one">
                     <div class="p-front__about__item__top">
                         <p class="p-front__about__item__index">01</p>
-                        <p class="p-front__about__item__title">高機能検索・マッチング</p>
+                        <h3 class="p-front__about__item__title">高機能検索・マッチング</h3>
                     </div>
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/front/about_01_img.svg">
                     <p class="p-front__about__item__text">大量データから瞬時に最適情報を抽出。営業機会を逃さない！</p>
@@ -64,7 +64,7 @@
                 <div class="p-front__about__item p-front__about__item--two">
                     <div class="p-front__about__item__top">
                         <p class="p-front__about__item__index">02</p>
-                        <p class="p-front__about__item__title">ノーコード・ローコード</p>
+                        <h3 class="p-front__about__item__title">ノーコード・ローコード</h3>
                     </div>
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/front/about_02_img.svg">
                     <p class="p-front__about__item__text">専門知識不足で誰でも素早く改善・施策反映が可能！</p>
@@ -73,7 +73,7 @@
                 <div class="p-front__about__item p-front__about__item--three">
                     <div class="p-front__about__item__top">
                         <p class="p-front__about__item__index">03</p>
-                        <p class="p-front__about__item__title">拡張性</p>
+                        <h3 class="p-front__about__item__title">拡張性</h3>
                     </div>
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/front/about_03_img.svg">
                     <p class="p-front__about__item__text">
@@ -98,9 +98,9 @@
                 <div class="p-front__features__item">
                     <div class="p-front__features__item__content">
                         <div class="p-front__features__item__tag">POINT 01</div>
-                        <div class="p-front__features__item__title">
+                        <h3 class="p-front__features__item__title">
                             <span>「人材マッチング」テンプレート</span>を標準搭載
-                        </div>
+                        </h3>
                         <p class="p-front__features__item__text">
                             人材業界での活用に最適化された候補者×求人のマッチングセットを標準装備。<br>
                             マッチング対象のオブジェクトや検索項目も初期構築済みのため、導入初日から即利用が可能です。貴社の業務フローや商習慣に応じたカスタマイズにも柔軟に対応します。
@@ -112,9 +112,9 @@
                 <div class="p-front__features__item">
                     <div class="p-front__features__item__content">
                         <div class="p-front__features__item__tag">POINT 02</div>
-                        <div class="p-front__features__item__title">
+                        <h3 class="p-front__features__item__title">
                             <span>柔軟なマッチングロジック</span>の<br class="sp">設定が可能
-                        </div>
+                        </h3>
                         <p class="p-front__features__item__text">
                             Salesforce上の任意の2オブジェクト間でマッチングが可能。<br>
                             「人材×案件」「企業×企業」「物件×希望条件」など、業界・業務を問わず利用できます。
@@ -127,9 +127,9 @@
                 <div class="p-front__features__item">
                     <div class="p-front__features__item__content">
                         <div class="p-front__features__item__tag">POINT 03</div>
-                        <div class="p-front__features__item__title">
+                        <h3 class="p-front__features__item__title">
                             条件・項目を<span>自由にカスタマイズ</span>
-                        </div>
+                        </h3>
                         <p class="p-front__features__item__text">
                             AND/OR条件・かっこ（括弧）を含む高度な論理検索式にも対応。検索項目や結果の表示内容も、コード不要で簡単に変更可能です。精度の高い絞り込みにより、“今すぐ使えるデータ”に瞬時にアクセスできます。
                         </p>
@@ -140,9 +140,9 @@
                 <div class="p-front__features__item">
                     <div class="p-front__features__item__content">
                         <div class="p-front__features__item__tag">POINT 04</div>
-                        <div class="p-front__features__item__title">
+                        <h3 class="p-front__features__item__title">
                             地理情報を活用した<br class="sp"><span>ローカルマッチング</span>にも対応
-                        </div>
+                        </h3>
                         <p class="p-front__features__item__text">
                             地図連携機能により、位置情報をベースにした検索やマッチングが可能。<br>
                             「1km圏内の案件」「特定エリアの登録者」など、視覚的に把握しながらデータ選定できます。<br>
@@ -155,9 +155,9 @@
                 <div class="p-front__features__item">
                     <div class="p-front__features__item__content">
                         <div class="p-front__features__item__tag">POINT 05</div>
-                        <div class="p-front__features__item__title">
+                        <h3 class="p-front__features__item__title">
                             Salesforce Flowとの連携で<br class="sp"><span>業務処理の自動化</span>
-                        </div>
+                        </h3>
                         <p class="p-front__features__item__text">
                             検索・マッチング結果に応じてSalesforce Flowを自動実行。
                             通知・レコード更新・Slack連携など、多様なビジネスロジックと組み合わせて、高度なワークフローを構築できます。<br>
@@ -189,25 +189,49 @@
     <section class="p-front__plan">
         <div class="l-inner">
             <h2 class="c-title--icon">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/clip.svg">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/clip_green.svg">
                 <div>
                     プラン
                 </div>
             </h2>
             <div class="p-front__plan__content">
-                <div class="p-front__plan__content--title">
-                    <p class="p-front__plan__content--title-target">CRM未導入のお客様</p>
-                    <p class="p-front__plan__content--title-plan">Default Plan</p>
-                    <p class="p-front__plan__content--title-user">１ユーザー</p>
-                    <p class="p-front__plan__content--title-price">¥2,000 JPY / 月</p>
+                <div class="p-front__plan__card p-front__plan__card--green">
+                    <div class="p-front__plan__card__header">
+                        <h3 class="p-front__plan__card__title">CRM未導入のお客様</h3>
+                        <p class="p-front__plan__card__plan">Edison Match</p>
+                        <p class="p-front__plan__card__user">
+                            <span>１ユーザー</span><br>
+                            ¥5,000 JPY / 月
+                        </p>
+                    </div>
+                    <ul class="p-front__plan__content--check">
+                        <li>30日間の無料トライアル</li>
+                        <li>初期費用：100,000円(税抜)</li>
+                        <li>最低契約期間：１年間</li>
+                        <li>最低契約数：５ユーザー</li>
+                        <li>ユーザー追加単価：１ユーザー</li>
+                        <li>ご契約の更新：１年間ごとに自動更新</li>
+                        <li>サポートサービス：メールによるテクニカル<br>サポート</li>
+                    </ul>
                 </div>
-                <div class="p-front__plan__content--check">
-                    <p>14日間の無料トライアル</p>
-                    <p>最低契約期間：１年間</p>
-                    <p>最低契約数：５ユーザー</p>
-                    <p>ユーザー追加単価：１ユーザー</p>
-                    <p>ご契約の更新：１年間ごとに自動更新</p>
-                    <p>サポートサービス：メールによるテクニカルサポート</p>
+                <div class="p-front__plan__card p-front__plan__card--blue">
+                    <div class="p-front__plan__card__header">
+                        <h3 class="p-front__plan__card__title">CRM未導入のお客様</h3>
+                        <p class="p-front__plan__card__plan">Edison Match ISV版</p>
+                        <p class="p-front__plan__card__user">
+                            <span>１ユーザー</span><br>
+                            ¥2,000 JPY / 月
+                        </p>
+                    </div>
+                    <ul class="p-front__plan__content--check">
+                        <li>14日間の無料トライアル</li>
+                        <li>初期費用：100,000円(税抜)</li>
+                        <li>最低契約期間：１年間</li>
+                        <li>最低契約数：５ユーザー</li>
+                        <li>ユーザー追加単価：１ユーザー</li>
+                        <li>ご契約の更新：１年間ごとに自動更新</li>
+                        <li>サポートサービス：メールによるテクニカル<br>サポート</li>
+                    </ul>
                 </div>
             </div>
         </div>
