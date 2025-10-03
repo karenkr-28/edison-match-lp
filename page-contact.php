529f887139e4651edc5632b758e6ccbd8437a8be
wakana1108 <sugitani@yukisaki.co.jp>
2025-10-03 14:13:41 +0900

    お問い合わせtページ修正

--- page-contact.php
+++ page-contact.php
@@ -23,28 +23,28 @@ get_header(); ?>
                 <input type="text">
             </div>
             <div class="p-contact__form__row">
-                <label for="">お名前　※</label>
+                <label for="">お名前 ※</label>
                 <input type="text" required>
             </div>
             <div class="p-contact__form__row">
-                <label for="">フリガナ　※</label>
+                <label for="">フリガナ ※</label>
                 <input type="text" required>
             </div>
             <div class="p-contact__form__row">
-                <label for="">メールアドレス　※</label>
+                <label for="">メールアドレス ※</label>
                 <input type="email" required>
             </div>
             <div class="p-contact__form__row">
-                <label for="">電話番号　※</label>
+                <label for="">電話番号 ※</label>
                 <input type="tel" required>
             </div>
             <div class="p-contact__form__row">
-                <label for="">お問い合わせ内容　※</label>
+                <label for="">お問い合わせ内容 ※</label>
                 <input type="textarea" required>
             </div>
             <input type="checkbox" required>
             <a href="<?php echo admin_url('/privacypolicy'); ?>">
-                「個人情報の取り扱いについて」</a>に同意する　※
+                「個人情報の取り扱いについて」</a>に同意する ※
 
             <button type="submit" class="p-contact__form__submit-btn">確認画面へ</button>
         </form>
